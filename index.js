d669d40084fe28513d28e44c262aefb7c2cd8212
shahan <shahan@gmail.com>
2023-05-29 14:03:25 +0500

    fix: app middleware in index.js to respond with json object instead of plain text

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ async function main() {
     res.locals.error = req.app.get("env") === "development" ? err : {}
     // render the error page
     res.status(err.status || 500)
-    res.send(message || 'Internal Server Error')
+    res.send({ error: message } || 'Internal Server Error')
   })
 
 
